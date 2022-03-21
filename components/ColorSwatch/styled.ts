211e773f00832c9eefb4cc11654320b67f00f398
Chayapol Chaipongsawalee <chayapol.cha@ku.th>
2022-03-21 15:01:59 +0700

    Add offset to color swatch

--- components/ColorSwatch/styled.ts
+++ components/ColorSwatch/styled.ts
@@ -17,13 +17,12 @@ const VariantColor = styled.div<Props>`
   justify-content: center;
   align-items: center;
   background-color: ${(props) => props.color};
-  border: ${(props) => (props.color === "#FFFFFF" ? "1" : "0")}px solid
-    ${(props) =>
-      props.color === "#FFFFFF" && !props.isSelected ? "#DEDEDE" : "#FFFFFF"};
+  border: 1px solid ${props => props.color=== "#FFFFFF" ? "#DEDEDE" : "transparent"};
   border-radius: 9999px;
   width: ${(props) => props.size}px;
   height: ${(props) => props.size}px;
   outline: 2px solid ${(props) => (props.isSelected ? "#BEBEBE" : "#ffffff")};
+  outline-offset: 2px;
   align-self: center;
   cursor: pointer;
   z-index: 999;
